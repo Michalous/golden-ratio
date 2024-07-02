51e1aade3428cf28e11c15a5093e5e214f12c089
Michalous <49964028+Michalous@users.noreply.github.com>
2024-07-02 22:18:08 +0100

    custom angle added

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var canvas = document.getElementById('myCanvas');
-    var ctx = canvas.getContext('2d');
+    var canvas = document.getElementById('myCanvas')
+    var ctx = canvas.getContext('2d')
+
+    var angleCanvas = document.getElementById('angleCanvas')
+    var angleCtx = angleCanvas.getContext('2d')
+    var inputValue
+    
+    document.getElementById('angleForm').addEventListener('submit', function(event) {
+        event.preventDefault()
+        inputValue = document.getElementById('angleInput').value
+        angleFunc(inputValue)
+        console.log(inputValue)
+    })
 
     var plotX = []
     var plotY = []
@@ -16,22 +27,50 @@ document.addEventListener('DOMContentLoaded', function() {
         angle += PHI
     }
     
-    console.log(plotX)
-    console.log(Math.cos((2 * Math.PI)))
-
     for (var i = 0; i < SIZE; i++) {
         (function(i) {
             setTimeout(function() {
                 drawDot(plotX[i], plotY[i], 2)
                 document.getElementById('iteration').innerHTML = i + 1
-            }, i * 100); // Delay each dot's drawing by i * 5000 milliseconds
-        })(i);
+            }, i * 100)
+        })(i)
     }
 
     function drawDot(x, y, r) {
-        ctx.beginPath();
-        ctx.arc(x, y, r, 0, 2 * Math.PI);
-        ctx.fillStyle = 'white';
-        ctx.fill();
+        ctx.beginPath()
+        ctx.arc(x, y, r, 0, 2 * Math.PI)
+        ctx.fillStyle = 'white'
+        ctx.fill()
     }
-});
+
+    function drawAngleDot(x, y, r) {
+        angleCtx.beginPath()
+        angleCtx.arc(x, y, r, 0, 2 * Math.PI)
+        angleCtx.fillStyle = 'yellow'
+        angleCtx.fill()
+    }
+
+    function angleFunc(angleValue) {
+        var plotAngleX = []
+        var plotAngleY = []
+        var SIZE = 300
+
+        var angleToDraw = angleValue
+    
+        for (var i = 0; i < SIZE; i++) {
+            plotAngleX.push(300 + 450*(Math.cos((2 * Math.PI) * angleToDraw) * i/500))
+            plotAngleY.push(300 + 450*(Math.sin((2 * Math.PI) * angleToDraw) * i/500))
+        
+            angleToDraw += angleValue
+        }
+        
+        for (var i = 0; i < SIZE; i++) {
+            (function(i) {
+                setTimeout(function() {
+                    drawAngleDot(plotX[i], plotY[i], 2)
+                    document.getElementById('angleIteration').innerHTML = i + 1
+                }, i * 100)
+            })(i)
+        }
+    }
+})
